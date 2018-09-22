a14f516fdefe063f1e2be2b147f60624f736f5c6
JeevithaSri <jeevithasri0107@gmail.com>
2018-09-22 15:37:15 +0530

    edit receiver and delete receiver

--- routes/commutter.php
+++ routes/commutter.php
@@ -10,4 +10,15 @@ Route::get('/home', function () {
     return view('commutter.home');
 })->name('home');
 
+Route::get('/addreceiver', 'commuttercontroller@showaddreceiver');
+Route::post('/addreceiver', 'commuttercontroller@addreceiver')->name('addreceiver');
+Route::get('/viewreceiver', 'commuttercontroller@viewreceiver');
+Route::delete('/viewreceiver/{id}/delete', 'commuttercontroller@deletereceiver')->name('deletereceiver');
+
+
+Route::get('/viewreceiver/{id}/edit', 'commuttercontroller@showEditreceiver')->name('editreceiver');
+Route::post('/viewreceiver/{id}/edit', 'commuttercontroller@updatereceiver')->name('updatereceiver');
+
+
+
 
