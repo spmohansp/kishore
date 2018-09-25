fba6c08be5b3670ec21a9392ee8fffafcab7afa8
Sudhakar Annadurai <asudhakar@live.in>
2018-09-25 12:32:32 +0530

    made corrections in hub and commutter

--- resources/views/hub/products/addproduct.blade.php
+++ resources/views/hub/products/addproduct.blade.php
@@ -25,19 +25,20 @@
                     </div>
                 </div><br> <br>
                 <div class="col-lg-6 u-mb-xsmall">
-                    <div class="c-field">
+                    <div class="r-field">
                         <label class="c-field__label" for="input2">Dimensions</label>
-                        <input class="c-input" id="input2" placeholder="Enter your dimensions" name="dimensions" required type="text">
+                        <input class="c-input" id="input2" placeholder="Enter your dimensions" name="dimensions" onkeypress="return  isNumberKey(event)"  required type="text">
                     </div>
                 </div><br><br>
                 <div class="col-lg-6 u-mb-xsmall">
                     <div class="c-field">
                         <label class="c-field__label" for="input3">Parcel Weight</label>
-                        <select class="c-select__input" name="parcelweight">
-                            <option value="volvo">under 1 kg</option>
-                            <option value="saab">5 kg</option>
-                            <option value="opel">10 kg</option>
-                            <option value="audi">15 kg</option>
+                        <select class="c-select__input" name="parcelweight" onkeypress="return  isNumberKey(event)"  required >
+                            <option value="under 1 kg">under 1 kg</option>
+                            <option value="2-5 kg">2-5 kg</option>
+                            <option value="6-10 kg">6-10 kg</option>
+                            <option value="11-15 kg">11-15 kg</option>
+                            <option value="over 15 kg">over 15 kg</option>
                         </select>
                     </div>
                 </div><br><br>
@@ -56,15 +57,14 @@
                 <div class="col-lg-6 u-mb-xsmall">
                     <div class="c-field">
                         <label class="c-field__label" for="input4">Pickup Date</label>
-                        <input class="c-input" id="input4" placeholder="Enter Pickupdate" name="pickupdate" required type="text">
+                        <input class="c-input" id="datepicker" placeholder="Enter pickupdate" name="pickupdate"onkeypress="return  isNumberKey(event)"  required >
                     </div>
-
                 </div>
                 <br><br>
                 <div class="col-lg-6 u-mb-xsmall">
                     <div class="c-field">
                         <label class="c-field__label" for="input4">Pickup Time</label>
-                        <input class="c-input" id="input4" placeholder="Enter animal pickuptime" name="pickuptime" required type="text">
+                        <input class="c-input timepicker" id="input4" placeholder="Enter pickuptime" name="pickuptime" onkeypress="return  isNumberKey(event)"  required>
                     </div>
 
                
@@ -74,7 +74,7 @@
 <div  style="text-align: center">
             <button class="c-btn c-btn--success u-mb-xsmall" type="submit">Next</button>&emsp;&emsp;&emsp;&emsp;
         
-            <button class="c-btn c-btn--success u-mb-xsmall" type="submit">Cancel</button>
+            <button class="c-btn c-btn--success u-mb-xsmall" type="reset">Cancel</button>
         
 </div>
 
@@ -82,3 +82,36 @@
 
 @endsection
 
+
+
+@section('script')
+                <script>
+                    function isNumberKey(evt){
+                        var charCode = (evt.which) ? evt.which : event.keyCode
+                        if (charCode > 31 && (charCode < 48 || charCode > 57))
+                            return false;
+                        return true;
+                    }
+                </script>
+                <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+                <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+                <script>
+                    $( function() {
+                        $( "#datepicker" ).datepicker();
+                    } );
+                </script>
+
+                <script src="//code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+                <script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
+                <script type="text/javascript">
+                    $(document).ready(function() {
+                        $('.timepicker').timepicker({
+                            timeFormat: 'HH:mm',
+                            interval: 60,
+                            defaultTime: '10',
+                        });
+                    });
+                </script>
+
+
+@endsection
--- resources/views/hub/products/editproduct.blade.php
+++ resources/views/hub/products/editproduct.blade.php
@@ -11,7 +11,7 @@
     Add product
 @endsection
 @section('content')
-    <form action="{{ route('hub.addproduct') }}" method="POST">
+    <form action="{{ route('hub.updateproduct',$product->id) }}" method="POST">
         {{ csrf_field() }}
 
         <h3>Add Parcel Details</h3>
@@ -21,41 +21,42 @@
                 <div class="col-lg-6 u-mb-xsmall">
                     <div class="c-field">
                         <label class="c-field__label" for="input1">Parcel Name</label>
-                        <input class="c-input" id="input1" placeholder="Enter your parcelname" name="parcelname" value = "{{ $addproduct -> parcelname }}"  required type="text" >
+                        <input class="c-input" id="input1" placeholder="Enter your parcelname" name="parcelname" value = "{{ $product -> parcelname }}"  required type="text" >
                     </div>
                 </div><br> <br>                <div class="col-lg-6 u-mb-xsmall">
                     <div class="c-field">
                         <label class="c-field__label" for="input2">Dimensions</label>
-                        <input class="c-input" id="input2" placeholder="Enter your dimensions" name="dimensions" value = "{{ $addproduct -> dimensions }}" required type="text">
+                        <input class="c-input" id="input2" placeholder="Enter your dimensions" name="dimensions" value = "{{ $product -> dimensions }}" onkeypress="return  isNumberKey(event)"  required type="text">
                     </div>
                 </div><br><br>
                 <div class="col-lg-6 u-mb-xsmall">
                     <div class="c-field">
                         <label class="c-field__label" for="input3">Parcel Weight</label>
-                        <select class="c-select__input" name="parcelweight" value = "{{ $addproduct -> parcelweight }}">
-                            <option value="volvo">under 1 kg</option>
-                            <option value="saab">5 kg</option>
-                            <option value="opel">10 kg</option>
-                            <option value="audi">15 kg</option>
+                        <select class="c-select__input" name="parcelweight" value = "{{ $product -> parcelweight }}" onkeypress="return  isNumberKey(event)"  required>
+                            <option value="under 1 kg">under 1 kg</option>
+                            <option value="2-5 kg">2-5 kg</option>
+                            <option value="6-10 kg">6-10 kg</option>
+                            <option value="11-15 kg">11-15 kg</option>
+                            <option value="over 15 kg">over 15 kg</option>
                         </select>
                     </div>
                 </div><br><br>
                 <div class="col-lg-6 u-mb-xsmall">
                     <div class="c-field">
                         <label class="c-field__label" for="input4">Pickup Address </label>
-                        <input class="c-input" id="input4"  type="text" placeholder="Enter your pickupaddresss"  name="pickupaddresss" value = "{{ $addproduct -> pickupaddresss }}" required >
+                        <input class="c-input" id="input4"  type="text" placeholder="Enter your pickupaddresss"  name="pickupaddresss" value = "{{ $product -> pickupaddresss }}" required >
                     </div>
                 </div><br><br>
                 <div class="col-lg-6 u-mb-xsmall">
                     <div class="c-field">
                         <label class="c-field__label" for="input4">Dropoff Address</label>
-                        <input class="c-input" id="input4" placeholder="Enter your dropoffaddress"  name="dropoffaddress"  value = "{{ $addproduct -> dropoffaddress }}" required type="text">
+                        <input class="c-input" id="input4" placeholder="Enter your dropoffaddress"  name="dropoffaddress"  value = "{{ $product -> dropoffaddress }}" required type="text">
                     </div>
                 </div><br><br>
                 <div class="col-lg-6 u-mb-xsmall">
                     <div class="c-field">
                         <label class="c-field__label" for="input4">Pickup Date</label>
-                        <input class="c-input" id="input4" placeholder="Enter Pickupdate" name="pickupdate" value = "{{ $addproduct -> pickupdate }}"required type="text">
+                        <input class="c-input" id="datepicker" placeholder="Enter Pickupdate" name="pickupdate" value = "{{ $product -> pickupdate }}"onkeypress="return  isNumberKey(event)"  required type="text">
                     </div>
 
                 </div>
@@ -63,7 +64,7 @@
                 <div class="col-lg-6 u-mb-xsmall">
                     <div class="c-field">
                         <label class="c-field__label" for="input4">Pickup Time</label>
-                        <input class="c-input" id="input4" placeholder="Enter animal pickuptime" name="pickuptime" value = "{{ $addproduct -> pickuptime }}"required type="text">
+                        <input class="c-input timepicker" id="input4" placeholder="Enter pickuptime" name="pickuptime" value = "{{ $product -> pickuptime }}" onkeypress="return  isNumberKey(event)"   required type="text">
                     </div>
 
                
@@ -81,4 +82,34 @@
 
 @endsection
 
-    
+            @section('script')
+                <script>
+                    function isNumberKey(evt){
+                        var charCode = (evt.which) ? evt.which : event.keyCode
+                        if (charCode > 31 && (charCode < 48 || charCode > 57))
+                            return false;
+                        return true;
+                    }
+                </script>
+                <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+                <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+                <script>
+                    $( function() {
+                        $( "#datepicker" ).datepicker();
+                    } );
+                </script>
+
+                <script src="//code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+                <script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
+                <script type="text/javascript">
+                    $(document).ready(function() {
+                        $('.timepicker').timepicker({
+                            timeFormat: 'HH:mm',
+                            interval: 60,
+                            defaultTime: '10',
+                        });
+                    });
+                </script>
+
+
+@endsection
