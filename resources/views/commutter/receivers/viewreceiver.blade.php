74cad24c108f3cb4d5c7ef4b230b57c6ddc13e35
spmohansp <spmohansp@gmail.com>
2018-09-25 13:06:58 +0530

    Icon Added and small changes in receiver module commutter

--- resources/views/commutter/receivers/viewreceiver.blade.php
+++ resources/views/commutter/receivers/viewreceiver.blade.php
@@ -1,35 +1,36 @@
 @extends('commutter.layout.master')
-@section('receivers')
+
+@section('receiver')
     is-active
 @endsection
 
-@section('addreceiver')
+@section('viewreceiver')
     is-active
 @endsection
 
 @section('header')
-    Add receiver
+    View receiver
 @endsection
+
 @section('content')
     <div class="row">
         <div class="col-12">
             <div class="c-table-responsive@wide">
                 <table class="c-table">
                     <thead class="c-table__head">
-                    <tr class="c-table__row">
-                        <th class="c-table__cell c-table__cell--head">Receiver Name</th>
-                        <th class="c-table__cell c-table__cell--head">Receiver's phone</th>
-                        <th class="c-table__cell c-table__cell--head">Receiver's Email Address</th>
-                        <th class="c-table__cell c-table__cell--head">Actions</th>
-                    </tr>
+                        <tr class="c-table__row">
+                            <th class="c-table__cell c-table__cell--head">Receiver Name</th>
+                            <th class="c-table__cell c-table__cell--head">Receiver's phone</th>
+                            <th class="c-table__cell c-table__cell--head">Receiver's Email Address</th>
+                            <th class="c-table__cell c-table__cell--head">Actions</th>
+                        </tr>
                     </thead>
-
                     <tbody>
                     @foreach($receivers as $receiver)
                         <tr class="c-table__row">
                             <td class="c-table__cell">{{$receiver->receivername}}</td>
-                            <th class="c-table__cell">{{$receiver->receiverphone}}</th>
-                            <th class="c-table__cell">{{$receiver->email}}</th>
+                            <td class="c-table__cell">{{$receiver->receiverphone}}</td>
+                            <td class="c-table__cell">{{$receiver->email}}</td>
                             <td class="c-table__cell">
                                 <div class="c-dropdown dropdown">
                                     <a href="#" class="c-btn c-btn--info has-icon dropdown-toggle" id="dropdownMenuTable1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" role="button">
