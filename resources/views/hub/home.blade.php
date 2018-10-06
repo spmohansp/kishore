cf137cc403689e046b6393ab6caca2e8f9a35a39
spmohansp <spmohansp@gmail.com>
2018-10-06 13:56:00 +0530

    hub map updated

--- resources/views/hub/home.blade.php
+++ resources/views/hub/home.blade.php
@@ -1,7 +1,7 @@
 @extends('hub.layout.master')
 
 @section('header')
-
+ 	Dashboard
 @endsection
 
 @section('dashboard')
@@ -9,6 +9,52 @@
 @endsection
 
 @section('content')
+ Dashboard
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC3_nchoqV696350i6DaDNW2WgQ42F2dRw&sensor=true&libraries=places" type="text/javascript"></script>
+ <div id="map" style="width: 100%; height: 400px;"></div>
+@endsection
+
+
+
+@section('script')
+  <script type="text/javascript">
+    var locations = [];
+		if(navigator.geolocation) {
+		   navigator.geolocation.watchPosition(showLocation);
+		} else {
+		   alert("Sorry,Something Went Wrong");
+		}
+
+         function showLocation(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+		      zoom: 14,
+		      center: new google.maps.LatLng(latitude,longitude),
+		      mapTypeId: google.maps.MapTypeId.ROADMAP
+		    });
+		    var infowindow = new google.maps.InfoWindow();
+
+		    var marker, i;
+
+		    for (i = 0; i < locations.length; i++) {  
+		      marker = new google.maps.Marker({
+		        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+		        type: 'info',
+		        map: map,
+		      });
+
+		      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+		        return function() {
+		          infowindow.setContent(locations[i][0]);
+		          infowindow.open(map, marker);
+		        }
+		      })(marker, i));
+		    }
+        }
+    </script>
+
 
 @endsection
 
